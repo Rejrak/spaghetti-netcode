ffb16c5fa93b7d2f7c002bcf31bb0a3e82da704e
Rejrak <lucchettiroberto95@outlook.com>
2025-09-08 18:08:30 +0200

    Refactor user attribute retrieval in session handler to return attributes and error, and update response message success condition based on user attributes

--- internal/actors/server/session_actor.go
+++ internal/actors/server/session_actor.go
@@ -7,6 +7,7 @@ import (
 	"net"
 	"spaghetti/internal/pkg/packets"
 	"spaghetti/internal/storage/sqlite"
+	"spaghetti/internal/user"
 	"time"
 
 	"github.com/anthdm/hollywood/actor"
@@ -62,14 +63,24 @@ func newSession(conn net.Conn) actor.Producer {
 	}
 }
 
-func (s *session) readUserAttributes(c context.Context, address string) {
+func (s *session) readUserAttributes(c context.Context, address string) (*user.Attributes, error) {
 	repo, err := sqlite.Open("./authblock.db")
 	if err != nil {
 		slog.Info("sqlite open error: %v", err)
+		return nil, err
 	}
 	s.repo = repo
-	userAttrs, updated, success, err := s.repo.GetAttrs(c, address)
-	slog.Info("Auth Request", "address", address, "attrs", userAttrs, "updated", updated, "success", success, "err", err)
+	userAttrs, updated, ok, err := s.repo.GetAttrs(c, address)
+	if err != nil {
+		slog.Error("Failed to get user attributes", "err", err)
+		return nil, err
+	}
+	slog.Info("Address Attrs", "attrs", userAttrs, "updated", updated, "ok", ok, "err", err)
+	if !ok {
+		s.repo.EnsureAddress(c, address, "")
+		return nil, fmt.Errorf("attributes not found")
+	}
+	return userAttrs, nil
 }
 
 func (s *session) Receive(c *actor.Context) {
@@ -80,17 +91,16 @@ func (s *session) Receive(c *actor.Context) {
 		slog.Info("new connection", "addr", s.conn.RemoteAddr())
 		go s.readLoop(c)
 	case actor.Stopped:
-		s.repo.Close()
 		s.conn.Close()
 	case *packets.CosmosPacket:
 		slog.Info("Handler: Received Cosmos packet:", msg)
 	case *packets.AuthMessage:
-		s.readUserAttributes(c.Context(), msg.Address)
+		userAttrs, _ := s.readUserAttributes(c.Context(), msg.Address)
 		resp := &packets.CosmosPacket{
 			SenderId: msg.Address,
 			Msg: &packets.CosmosPacket_ResponseMessage{
 				ResponseMessage: &packets.ResponseMessage{
-					Success: true,
+					Success: userAttrs.CanCreate,
 					Message: "",
 				},
 			},
