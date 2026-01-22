e118a71875b7088329d5cc8d6f38b5588d2f5f45
Rejrak <lucchettiroberto95@outlook.com>
2026-01-22 12:28:57 +0100

    test

--- internal/actors/server/server_actors.go
+++ internal/actors/server/server_actors.go
@@ -79,6 +79,7 @@ func (s *server) startSyncronizer(c *actor.Context) {
 		KeycloakBaseURL:             cfgData.KeycloakBaseURL,
 		KeycloakRealm:               cfgData.KeycloakRealm,
 		KeycloakClientID:            cfgData.KeycloakClientID,
+		KeycloakClientSecret:        cfgData.KeycloakClientSecret,
 		KeycloakEnableWalletLookup:  cfgData.KeycloakEnableWalletLookup,
 		KeycloakWalletAttributeName: cfgData.KeycloakWalletAttributeName,
 		// KeycloakWalletAttributeName: "walletAddress", // default già gestito
@@ -107,6 +108,7 @@ func (s *server) Receive(c *actor.Context) {
 				KeycloakBaseURL:             "http://localhost:8080",
 				KeycloakRealm:               "cosmos",
 				KeycloakClientID:            "spaghetti-service",
+				KeycloakClientSecret:        "nA3XmI7wgHnxdXepKGgMkJz66tyUbviJ",
 				KeycloakWalletAttributeName: "",
 				KeycloakEnableWalletLookup:  true,
 
